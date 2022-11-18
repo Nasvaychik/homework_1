a9f90f4ea2dd7b7aab98789c7ec158668682a8be
Nasvaychik <zuoow12345@mail.ru>
2022-11-18 20:38:51 +0300

    Homework_1

---
+++ Homework_1/script.js
@@ -0,0 +1,16 @@
+const student = "Матвеев Кирилл Дмитриевич"; // Очевидно, что здесь ваши личные Фамилия, Имя и Отчество
+
+document.getElementById("student").innerHTML = student;
+
+let truescore = ["2:5"];
+let issue = ["3:4"];
+let score= prompt('Предпологаемый счёт');
+if (score ==truescore) alert ('Счёт был угадан верно!');
+else if (score==issue) alert ('Счёт не угадали, но исход угадали')
+else alert ('Ни счёта, ни исхода не угадали')
+console.log( truscore );
+
+
+
+
+
